ccd0850540c01be336f1a01eb33e465bfc31376e
cazuzaneves <cazuzaneves@gmail.com>
2020-07-21 22:48:51 -0300

    end

--- src/clojure_exercices_ch5/core.clj
+++ src/clojure_exercices_ch5/core.clj
@@ -5,3 +5,36 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(defn sum
+  ([vals] (sum vals 0))
+  ([vals acumulated-val]
+   (if (empty? vals)
+     acumulated-val
+     (recur (rest vals) (+ acumulated-val (first vals))))))
+
+(sum [8 8 9])
+
+(defn two-comp
+  [f g]
+  (fn [& args]
+    (f (apply g args))))
+
+(def two-comp-res (two-comp inc +))
+
+(two-comp-res 2 5)
+
+(defn mult-comp
+  [& funcs]
+  (fn [& args]
+    (loop [[f & remaining-funcs] (reverse funcs) new-arg args]
+      (let [result (if (seq? new-arg) (apply f new-arg) (f new-arg))]
+        (if (empty? remaining-funcs)
+          result
+          (recur remaining-funcs result))))))
+
+(def mult-comp-res (mult-comp inc inc #(/ % 2.0) +))
+
+(mult-comp-res 2 5)
+
